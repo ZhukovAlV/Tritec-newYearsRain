67b504c17fda8beb903ca028d3939dc0adcd93e9
ZhukovAlV <Olenka2012$github>
2022-01-06 21:36:36 +0600

    Исправлена ошибка с выставлением сложности игры

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -8,21 +8,28 @@ public class Game {
     // Главный метод, который запускает игру
     public static void main(String[] args) {
         String rez;
-        int slogn;
+        int slogn = 0;
+
         // Сложность игры
         rez = JOptionPane.showInputDialog(null,
                 "Введите сложность игры от 1 до 7",
                 "Сложность игры", JOptionPane.INFORMATION_MESSAGE);
-        if (rez == null || rez.equals("")) slogn = 0;
-        else slogn = Integer.parseInt(rez);
 
         // Проверка на ввод корректной сложности игры
-        if(slogn >= 1 && slogn <= 7) {
-            Okno window = new Okno(slogn);
-        } else {
-            rez = JOptionPane.showInputDialog(null,
-                    "Вы ввели некорректную сложность игры",
-                    "Введите заново", JOptionPane.INFORMATION_MESSAGE);
+        while(rez == null || rez.equals("") || slogn < 1 || slogn > 7) {
+            // Переводим строку с число
+            try {
+                slogn = Integer.parseInt(rez);
+                if (slogn < 1 || slogn > 7) throw new NumberFormatException();
+            } catch (NumberFormatException e) {
+                rez = JOptionPane.showInputDialog(null,
+                        "Вы ввели некорректную сложность игры",
+                        "Введите заново", JOptionPane.INFORMATION_MESSAGE);
+            }
         }
+
+        // Если все нормально введено, то просто окно игры создаем (передаем в него сложность игры)
+        Okno window = new Okno(slogn);
     }
+
 }
